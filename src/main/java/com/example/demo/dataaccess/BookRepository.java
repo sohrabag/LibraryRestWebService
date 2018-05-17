dfe1328d2ca64aebbaaf033f28a2b31902d2bf97
Unknown <azamighadim2@gmail.com>
2018-05-17 15:38:15 +0200

    trying to use JPQL SPRING DATA JPA AND STORED PROCEDURES

--- src/main/java/com/example/demo/dataaccess/BookRepository.java
+++ src/main/java/com/example/demo/dataaccess/BookRepository.java
@@ -50,4 +50,41 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 //     
 //    @Async
 //    Future<Stream<Todo>> findByTitle(String title);
+	
+//stored procedures and how to use them: FROM SPRING DATA JPA
+//	CREATE OR REPLACE PACKAGE test_pkg AS
+//	   PROCEDURE in_only_test (inParam1 IN VARCHAR2);
+//	   PROCEDURE in_and_out_test (inParam1 IN VARCHAR2, outParam1 OUT VARCHAR2);
+//	END test_pkg;
+//	/
+//	CREATE OR REPLACE PACKAGE BODY test_pkg AS
+//	   PROCEDURE in_only_test(inParam1 IN VARCHAR2) AS
+//	   BEGIN
+//	      DBMS_OUTPUT.PUT_LINE('in_only_test');
+//	   END in_only_test;
+//	   PROCEDURE in_and_out_test(inParam1 IN VARCHAR2, outParam1 OUT VARCHAR2) AS
+//	   BEGIN
+//	      outParam1 := 'Woohoo Im an outparam, and this is my inparam ' || inParam1;
+//	   END in_and_out_test;
+//	END test_pkg;
+	
+//THIS IS HOW CLASS IS ANNOTATED
+//
+//@Entity
+//@Table(name = "MYTABLE")
+//@NamedStoredProcedureQueries({
+//   @NamedStoredProcedureQuery(name = "in_only_test", 
+//                              procedureName = "test_pkg.in_only_test",
+//                              parameters = {
+//                                 @StoredProcedureParameter(mode = ParameterMode.IN, name = "inParam1", type = String.class)
+//                              }),
+//   @NamedStoredProcedureQuery(name = "in_and_out_test", 
+//                              procedureName = "test_pkg.in_and_out_test",
+//                              parameters = {
+//                                 @StoredProcedureParameter(mode = ParameterMode.IN, name = "inParam1", type = String.class),
+//                                 @StoredProcedureParameter(mode = ParameterMode.OUT, name = "outParam1", type = String.class)
+//                              })
+//})
+//public class MyTable implements Serializable {
+//}
 }

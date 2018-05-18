c7022ff08d83a4ff58d7a6db313524aa0edc51b9
sag sag <sohrabag@kth.se>
2018-05-18 07:05:54 +0200

    java docs are added to author class

--- src/main/java/com/example/demo/controllers/AuthorController.java
+++ src/main/java/com/example/demo/controllers/AuthorController.java
@@ -35,27 +35,48 @@ public class AuthorController {
 	/**
 	 * create api creates a new author in the author table
 	 * author parameter passes in the properties for new author
-	 * @param author
-	 * @return
+	 * annotation PostMapping marks this method as CRUD operation 
+	 * that can be calld from any client like postman, web broswer 
+	 * or a mobile device
+	 * @param author is an input parameter and entire object must be passed in as JSON type
+	 * @return is a object of type Author
 	 */
 	@PostMapping(value = "/authors/addAuthor")
 	public Author create( @Valid @RequestBody Author author)
 	{
 		return ams.create(author);
 	}
-	
+	/**
+	 * Annotation GetMapping idetifies this url as a GET operation of the CRUD operations
+	 * read is a CRUD api and it returns an Author object in JSON format
+	 * @param authorId is the input parameter of type int that is passed in by client
+	 * @return is an object of type Author. entire object will be returned as a JSON type
+	 */
 	@GetMapping("/authors/{authorId}")
 	public Author read(@PathVariable int authorId)
 	{
 		return ams.read(authorId);
 	}
 	
+	/**
+	 * Annotation PutMapping identifies that this api is a update operation of CRUD operations
+	 * the uri is in the double quotation marks
+	 * @param authorId : this is input parameter which is of type int and in Author id to be update
+	 * @param changedAuthor : this is input parameter of type Author object which is passed as JSON type
+	 * @return is a object of type Author which the update object of type JSON
+	 */
 	@PutMapping("/author/update/{authorId}")
 	public Author updageAuthorById(@PathVariable int authorId, @RequestBody Author changedAuthor)
 	{
 		return ams.update(authorId, changedAuthor);
 	}
 	
+	/**
+	 * Annotation DeleteMapping idetifies this api as a delete operation of CRUD operations
+	 * @param authorId : input parameter of type int which identifies id of author to be delete
+	 * @return : returns an ResponsEntity object which is a conmprehensive information about whether 
+	 * delete operation has been successful.
+	 */
 	@DeleteMapping("/authors/delete/{authorId}")
 	public ResponseEntity<?> delete(@PathVariable int authorId)
 	{

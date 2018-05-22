80393b0c25950b9948592eb4784a0b38e4bbf5c5
Unknown <azamighadim2@gmail.com>
2018-05-22 16:44:26 +0200

    adding thymeleaf and html files to the project

---
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/")
+	public ModelAndView homePage()
+	{
+		return new ModelAndView("first");
+	}
+}

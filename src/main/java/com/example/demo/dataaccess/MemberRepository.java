e9a5664d72d0cf50407831e03e06b2b60fca9f86
Unknown <azamighadim2@gmail.com>
2018-06-07 16:45:59 +0200

    modified MemberRepository.java to select
    a range of members only

--- src/main/java/com/example/demo/dataaccess/MemberRepository.java
+++ src/main/java/com/example/demo/dataaccess/MemberRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.dataaccess;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+	@Query("SELECT m FROM Member m where m.id BETWEEN :id1 AND :id2")
+	List<Member> findAllByIdRange(@Param("id1") Integer id1, @Param("id2") Integer id2);
 	
+//	findReferenceFieldBetween(value1,value2);
 }

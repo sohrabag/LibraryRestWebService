be13b17c86a1991ab834d2fd592db67eb30c4a10
Unknown <azamighadim2@gmail.com>
2018-06-12 16:22:52 +0200

    created members1.html and put all the java script code of it
    inside application.js and trying to search the member table to fill the table mytable with the query results.

--- src/main/resources/templates/application.js
+++ src/main/resources/templates/application.js
@@ -1,129 +1,320 @@
-/* application.js */
-
-//$(document).ready(function() {
-//	//create a <p> node with the price
-//
-//	//to add this tag to the DOM tree we have following methods:
-//	//.append(<element>), .prepend(<element>), .after(<element>), .before(<element>)
-//	/*.appendTo(<element>), .prependTo(<element>), .insertAfter(<element>), 
-//							.insertBefore(<element>)*/
-//
-//	$('button').on('click', function(){
-//		var price = $('<p>From $399.99</P>');
-//		//run this function on click
-//		$(this).closest('.vacation').append(price);
-//		$(this).remove();
-//	});
-//
-//});
-
 $(document).ready(function() {
 
-//                      var xhr = new XMLHttpRequest();
-//                      var open_str = "http://localhost:8080/loans/members/1/all";
-//                      xhr.open("GET", open_str, true);
-//                      xhr.onreadystatechange = function() {
-//                                  alert(xhr.readyState + "" + xhr.status);
-//                        if (xhr.readyState == 4 && xhr.status == 200) 
-//                        {
-//
-//                            alert("session opend success");
-//
-//                            var json = JSON.parse(xhr.responseText);
-//                            //alert(JSON.stringify(json, null, 10));
-//                            console.log(JSON.stringify(json, null, 10));
-//                            /* The following code is a Javascript + JQuery code to call a rest web server */
-//                            $('#results').text('');
-//                            		
-//                            for(var index in json)
-//                           	{
-//                           		$('#results').append('<p>' + json[index].book.title + '</p>');
-//                           	}
-//                            
-//                        	}
-//                      }
-//  console.log("before xhr.send(null)");
-//  xhr.send(null);
-                      /* 
-                       * The following code is calling AJAX query by using JQuery which calls a rest controller web service 
-                       * using AJAX theory to make the client side more responsive to the user without slowing 
-                       * down the client application. 
-                       * http://localhost:8080/loans/all
-                       * 
-                       * 
-                       */
- 
-// 						var x = document.getElementById("searchBox");
-//						x.value = '1';
-//						console.log(parseInt(x.value));
-						var memid = $('#searchBox').val();
-						
-						if(memid.length === 0)
-							{
-								console.log("The memberId string is empty");
-							}
-						else
-							{
-								$.getJSON("http://localhost:8080/loans/members/memberId/all", 
-	                      				{ memberId: parseInt($('#searchBox').val()) }, 
-	                      				function(data) {
-	                      					console.log("Ajax Succeeded to Call Rest Controller");
-	                      					console.log("data received " + data);
-	                      					$("#results").text('');
-	                      					for(var index in data)
-	                      						{
-	                      							$("#results").append('<p>' + data[index].book.title + '</p>');
-	                      						}
-	                      				});
-		                        	console.log(parseInt($('#searchBox').val()));
-							}
-
-//						$.getJSON("http://localhost:8080/loans/all",  function(data) {
-//								console.log("success calling the rest controller");
-//								alert(JSON.stringify(data, null, 10));
-//								$("#results").text('');
-//								for(var index in data)
-//									{
-//										$("#results").append('<p>' +data[index].book.title + '</p>');
-//									}
-//							console.log("before calling JSON.stringify(data)");
-//							alert(JSON.stringify(data, null, 10));
-//							console.log("after calling JSON.stringify(data)");
-//								var items = [];
-//								$.each(data, function( key, val ) {
-//									items.push( "<li id='" + key + "'>" + val + "</li>");
-//								});
-//								
-//								$( "<ul/>" , {
-//									"class": "my-new-list", 
-//									html: items.join("")
-//								}).appendTo( "body" );
-//						});
+	var meminfo = new Array("","","","","");
+	var bDone = false;
+	
+	var jsond;
+	var col_names;
+	var table1;
+	
+	/* when document is loaded the table headers must be initialized */
+	/* window.onload = function(){
+		fillTableHeader();
+	} */
+	
+	$(document).ready(function(){
+		fillTableHeader();
+	});
 });
-
-function onKeyUpHandler()
-{
-	var memid = $('#searchBox').val();
-	var id = parseInt(memid);
-	var url = "http://localhost:8080/loans/members/" + id + "/all";
-
-	if(memid.length === 0)
-	{
-		console.log("The memberId string is empty");
+	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+	function myFunction() {
+	    var x = document.getElementById("myTopnav");
+	    if (x.className === "topnav") {
+	        x.className += " responsive";
+	    } else {
+	        x.className = "topnav";
+	    }
 	}
-	else
+	
+	function getMemInfo()
 	{
-		$.getJSON(url, 
-				{ memberId: id }, 
-				function(data) {
-					console.log("Ajax Succeeded to Call Rest Controller");
-					console.log("data received " + data);
-					$("#results").text('');
-					for(var index in data)
-						{
-							$("#results").append('<p>' + data[index].book.title + '</p>');
-						}
+		if(0 == document.getElementById("name").value)
+		{
+			alert("enter a value in the first name box");
+			return false;
+		}
+		else if(0 == document.getElementById("dob").value)
+		{
+			alert("please enter date of birth for the member");
+			return false;
+		}
+		else if(0 == document.getElementById("address").value)
+		{
+			alert("please enter address for the member");
+			return false;
+		}
+		else if(0 == document.getElementById("email").value)
+		{
+			alert("please enter email for the member");
+			return false;
+		}
+		else if(0 == document.getElementById("phone").value)
+		{
+			alert("please enter phone number for the member");
+			return false;
+		}
+		
+		meminfo[0] = document.getElementById("name").value;
+		meminfo[1] = document.getElementById("dob").value;
+		meminfo[2] = document.getElementById("address").value;
+		meminfo[3] = document.getElementById("email").value;
+		meminfo[4] = document.getElementById("phone").value;
+	}
+	
+	/*
+	 * function update the database when we add a member
+	 */
+	function updDB()
+	{
+	 	var mem_data = serialTheData();
+			console.log(mem_data);
+			var json_data = JSON.stringify(mem_data);
+			console.log(json_data);
+			return new Promise((resolve, reject) => {
+				$.ajax({
+					type: 'post',
+					url: 'http://localhost:8080/members/addMember/',
+					data: json_data,
+					success: function(data){console.log('data' + data);},
+					contentType: "application/json",
+					dataType: 'json'
+					})
+					.done(function(){
+	 					bDone = true;
+	 					console.log("inside serialTheData.done bDone is: " + bDone);
+	 					document.getElementById("status").innerHTML="Operation succeeded";
+						return resolve(json_data);
+					})
+					.fail(function(error){
+							console.error(error);
+	 						bDone = false;
+	 						console.log("inside serialTheData.fail bDone is: " + bDone);
+							return reject(error);
+					})
+					.always(function(){
+								//called after done or failed
+								console.log(bDone);
+							});
 				});
-			console.log(parseInt($('#searchBox').val()));
+	 }
+	
+	/*
+	 * The function updates the database when a delete happens
+	 */
+	function delDB()
+	{
 	}
-}
+	
+	/* .serialize() in jQuery to JSON format data of the members form */
+	function getFormData($form){
+	    var unindexed_array = $form.serializeArray();
+	    var indexed_array = {};
+	
+	    $.map(unindexed_array, function(n, i){
+	        indexed_array[n['name']] = n['value'];
+	    });
+	
+	    return indexed_array;
+	}
+	
+	/* .serialize() in jQuery to JSON format data of the members form */
+	function serialTheData()
+	{
+		var $form =  $("#form_data");
+		var data = getFormData($form);
+		console.log(data);
+		return data;
+	}
+	
+	function addFunc()
+	{
+			updDB();
+			
+			var table = document.getElementById("myTable");
+			var rsize = table.rows.length;
+			console.log("number of rows is: " + rsize);
+			var csize = table.rows[0].cells.length;
+		 	console.log("number of columns is: " + csize);
+	
+			//get the memeber info from the form if all the inputs are filled in the formula
+			var ret = getMemInfo();
+			if(ret == false)
+				return false;
+	
+	/* 		if(bDone == true){ */
+					
+				console.log("inside adding a new row to the table.");
+				var row = table.insertRow(rsize);
+				var cell;
+				
+				for(var i=0; i < csize; i++)
+				{
+					cell = row.insertCell(i);
+					cell.innerHTML = meminfo[i];
+				}
+	/*  		}
+	 */
+	 }
+	
+	
+	function fillTableHeader()
+	{
+		table1 = document.getElementById("myTable");
+		var url1 = 'http://localhost:8080/members/getRange/1/1';
+	
+		//set crossDomain: true, option to true to allow cross domain requests such as(SSONP) on the same domain
+		$.getJSON( url1,
+				function(data){
+					console.log("data is: " + JSON.stringify(data));
+					console.log("data is: " + JSON.stringify(data));
+					jsond = JSON.stringify(data);
+					console.log("data jsond is: " + jsond);
+					console.log(jsond.length);
+	
+					if(jsond.length > 0)
+					{
+						var cols = JSON.parse(jsond);
+						var names = cols[0];
+						col_names = Object.getOwnPropertyNames(names);
+					}
+	
+					console.log(col_names.length);
+					/* find the index for the last row */
+				 	var rows = table1.rows.length;
+				 	/* insert the new row after the last row */
+				 	var row = table1.insertRow(rows);
+					var cell;
+				 	var csize = col_names.length;
+	
+					for(var i=0; i < csize; i++)
+					{
+						cell = row.insertCell(i);
+						cell.innerHTML = col_names[i];
+					}
+												
+			});
+	
+	}
+	
+	//searches for the members of a range
+	function searchFunc()
+	{
+	/* 	var $form =  $("#search_data"); */
+	
+		
+		//build data with JSON format
+		//make sure each input has an attribute name
+		//both enabled and disabled inputs in the form formula is captured
+		//serializeArray only captures enabled inputs in the form formula!! so it is not safe to use that in all cases.
+		var range = new Array(2);
+		var inputs = $("#search_data    : input");
+		var obj = $.map(inputs, function(n, i){
+			var o = {};
+	
+			o[n.name] = parseInt($(n).val());
+			range[i] = parseInt($(n).val());
+			return o;
+			});
+	/* 
+		var formdata = $("#search_data").serializeArray();
+		console.log(formdata);
+		var data = {};
+		$(formdata).each(function(index, obj){
+			data[obj.name] = obj.value;
+			}); 
+		console.log(data);
+		var json_data = JSON.stringify(data);
+		console.log(json_data);
+	*/
+	
+	console.log(obj);
+	
+	var json_data = JSON.stringify(obj);
+	console.log(json_data);
+	var url1 = 'http://localhost:8080/members/getRange/' + range[0];
+	console.log(url1);
+	url1 = url1 + '/' + range[1];
+	
+	//set crossDomain: true, option to true to allow cross domain requests such as(SSONP) on the same domain
+	$.getJSON( url1,
+			function(data){
+				console.log("data is: " + JSON.stringify(data));
+				});
+				 
+	  $.ajax({
+		type: 'get',
+			url: url1,
+	//		contentType: "application/json",
+			dataType: 'json',
+			success: function (data) {
+					
+					console.log("data is: " + JSON.stringify(data));
+					jsond = JSON.stringify(data);
+					console.log("data jsond is: " + jsond);
+					console.log(jsond.length);
+	
+					if(jsond.length > 0)
+					{
+						var cols = JSON.parse(jsond);
+						var names = cols[0];
+						col_names = Object.getOwnPropertyNames(names);
+					}
+			 }
+		})				
+		.done(function(){
+					console.log(".done is called..")
+			})
+			.fail(function(error){
+					console.error(error);
+			})
+			.always(function(){
+						//called after done or failed
+						console.log(".always is called..")
+	
+					});
+	
+		/* fill the result of the search in the table */
+	  fillMemberTable();
+	}
+	
+	function fillMemberTable()
+	{
+		console.log("from inside fillMemberTable()..");
+	 	var table = document.getElementById("myTable");
+		var rsize = table.rows.length;
+		var csize = table.rows[0].cells.length;
+		console.log("number of rows is: " + rsize);
+		console.log("number of columns is: " + csize);
+		console.log(jsond);
+	 
+	/* 	//iterate through member list and insert it
+		for(var index i jsond)
+		{
+			for(var i = 0; i < csize; i++)
+			{
+				cell = row.insertCell(i);
+				cell.innerHTML = jsond[index][i];
+			}
+		} */
+							
+	}
+	
+	function modifyFunc()
+	{
+	/* 	console.log(col_names);	
+		for(var key in col_names)
+		{
+			console.log(col_names[key]);
+		} 
+	*/
+		fillTableHeader();
+	}
+	
+	function deleteFunc()
+	{
+		
+	}
+	
+	var x = serialTheData();
+	
+
